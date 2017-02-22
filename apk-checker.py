1545011b0f5f27d450c2843f26772c24aab33588
Sean.Zhou <sean.zhou@ef.com>
2017-02-22 13:07:22 +0800

    First commit

---
+++ apk-checker.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import re
+import hashlib
+
+
+def getBaseInfo(apkpath, md5_to_check=""):
+    print 80 * '-'
+
+    # check info | egrep 'package|application-label-zh-CN'
+    result = os.popen("./aapt d badging %s  " % apkpath).read()
+
+    match = re.compile(
+        "package: name='(\S+)' versionCode='(\d+)' versionName='(\S+)' ").match(result)
+    if not match:
+        raise Exception("AAPT can't get packageinfo")
+
+    packagename = match.group(1)
+    versioncode = match.group(2)
+    versionname = match.group(3)
+
+    #print result
+    sub = "application-label-zh-CN"
+    startpos = result.index(sub)
+    endpos = result.index("'", startpos + len(sub) + 2)
+
+    print "package: name=%s, versionCode=%s, versionName=%s" % (packagename, versioncode, versionname)
+    print (result[startpos:endpos])
+
+    file = open(apkpath, "rb")
+    md5_checksum = hashlib.md5(file.read()).hexdigest()
+    file.close()
+
+    print "Generated md5:", md5_checksum
+
+    if md5_to_check != "":
+        print "Equal to given MD5 ? ", md5_checksum == md5_to_check
+
+    print 80 * '-'
+
+
+def getCurrentDirApk():
+    # Gets the apk file under current folder.
+    for dir in os.walk(os.curdir):
+        for filename in dir[2]:
+            if os.path.splitext(filename)[1] == '.apk':
+                print('find apk:', filename)
+                return filename
+
+
+if __name__ == '__main__':
+    paramcnt = len(sys.argv)
+    md5_origin = ""
+
+    if paramcnt == 1:
+        apkName = getCurrentDirApk()
+
+    elif paramcnt == 2:
+        apkName = sys.argv[1]
+
+    elif paramcnt == 3:
+        apkName = sys.argv[1]
+        md5_origin = sys.argv[2]
+    else:
+        usage = "Usage: python apk-checker.py [full-path-to-apk-file] [file-md5-to-check]"
+        print usage
+        exit()
+
+    if not apkName:
+        print('can not find apk!!!')
+        exit()
+
+    getBaseInfo(apkName, md5_origin)
